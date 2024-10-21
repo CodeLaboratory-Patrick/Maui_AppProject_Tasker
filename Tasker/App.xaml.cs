e60b709eb14752f21c070f985fefcb08f64be92d
Donghwi_Park <gptnc5100@gmail.com>
2024-10-21 10:18:09 +0200

    Creating the project and the initial structure.

--- Tasker/App.xaml.cs
+++ Tasker/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Tasker
+﻿using Tasker.MVVM.Views;
+
+namespace Tasker
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new MainView();
         }
     }
 }
